114cc1aed8c9fa53510616a1faec832875dedf77
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-09-25 17:01:29 +0100

    🎨 Styling Complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import MetaTags from 'components/MetaTags';
 import Divider from 'components/Divider';
-import Prism from "prismjs";
-import "prismjs/themes/prism-tomorrow.css";
-import React, { useEffect } from "react";
+import Code from 'components/Code';
 
 const snippetCode = `
-const App = props => {
+export default function App(props) {
 	return (
 		<div>
 		<h1> Monad </h1>
@@ -15,39 +13,44 @@ const App = props => {
 };`;
 
 export default function Home() {
-	useEffect(() => {
-		Prism.highlightAll();
-	  }, []);
-
 	return (
 		<>
 			<MetaTags />
-			<section>
+			<section className='left'>
 				<div id='content'>
-					<h1 className="header">Share <span className="snippets">snippets</span> in seconds</h1>
+					<h1 className='header'>
+						Share <span className='blue'>snippets</span> in seconds
+					</h1>
 				</div>
 				<Divider />
 			</section>
 			<section className='foreground right'>
-				<pre>
-					<code className="language-js">{snippetCode}</code>
-				</pre>
-				<div id='content'>
-					<p className="badge green">Built Socially</p>
-					<h1>Share With Everyone</h1>
-					<p>
-					It has never been easier to assign tags and share your short code snippets with the dev community. Monad was created with SEO in mind so your snippets can always be found!
-					</p>
+				<div id='content' className='inline'>
+					<pre>
+						<Code language='jsx' code={snippetCode} />
+					</pre>
+					<span className='text'>
+						<p className='badge green'>Built Socially</p>
+						<h1>Share With Everyone</h1>
+						<p>
+							It has never been easier to assign tags and share your short code snippets
+							with the dev community. Monad was created with SEO in mind so your snippets
+							can always be found!
+						</p>
+					</span>
 				</div>
 				<Divider foreground={true} />
 			</section>
-			<section>
+			<section className='left'>
 				<div id='content'>
-					<p className="badge pink">Collaborate With Ease</p>
-					<h1>Work in Private</h1>
-					<p>
-						Easily share snippets with everyone in Monad, or just a couple people you trust. Keep your snippets unlisted, or public to anyone to view.
-					</p>
+					<span className='text'>
+						<p className='badge red'>Collaborate With Ease</p>
+						<h1>Work in Private</h1>
+						<p>
+							Easily share snippets with everyone in Monad, or just a couple people you
+							trust. Keep your snippets unlisted, or public to anyone to view.
+						</p>
+					</span>
 				</div>
 				<Divider />
 			</section>
