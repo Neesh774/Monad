3d70d767a6acbb98972399ad3a7a9ecda41e35e7
Neesh774 <kanishq0106@gmail.com>
2021-12-05 10:33:34 -0500

    forgot about a thing

--- components/langs.tsx
+++ components/langs.tsx
@@ -42,7 +42,6 @@ export const langs: lang[] = [
     extension: () => javascript({ jsx: true }),
     file: "jsx",
     name: "React Javascript",
-    color: 54,
   },
   {
     extension: () => javascript({ typescript: true }),
