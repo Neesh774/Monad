fd91c705a248eebb14c2272f141eb82dc0349f45
RiverRipple <51027259+RiverRipple@users.noreply.github.com>
2022-01-13 07:47:18 -0500

    vulnerability fixes

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,22 @@
-import Link from 'next/link';
+import Link from "next/link";
+import MetaTags from "components/MetaTags";
 
 export default function Lost() {
-	return (
-		<div className="lost-parent">
-            <div className="vertical">
-                <h1 className="pink">404</h1>
-                <h2>
-                    Looks like you ended up somewhere you shouldn&apos;t be! <br />
-                    <Link href="/">
-                        <a className="home-link">Here&apos;s the way home</a>
-                    </Link>
-                </h2>
-                <img src={'/404.svg'} alt="404" />
-            </div>
+  return (
+    <>
+      <MetaTags title="404 NOT FOUND" description="" />
+      <div className="lost-parent">
+        <div className="vertical">
+          <h1 className="pink">404</h1>
+          <h2>
+            Looks like you ended up somewhere you shouldn&apos;t be! <br />
+            <Link href="/">
+              <a className="home-link">Here&apos;s the way home</a>
+            </Link>
+          </h2>
+          <img src={"/404.svg"} alt="404" />
         </div>
-	);
+      </div>
+    </>
+  );
 }
